a52f45ee7b359540827fbc693d0568e5daae901f
Jabir Lopez <jlopez@itzamedia.com>
2024-08-05 22:11:09 +0100

    changes in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from "astro/config";
-import { lifecycleLogs } from "src/integrations/lifecycle-logs";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), lifecycleLogs()],
+  integrations: [react()],
 });
