b6b33d26900d40314261fc8f3fe36ef4bbfb62c9
Jabir Lopez <jlopez@itzamedia.com>
2024-08-06 18:29:44 +0100

    bit of GSAP added

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,15 +1,65 @@
 import styles from "./styles/Features.module.scss";
+import { useInView } from "react-intersection-observer";
+// @ts-ignore
+import { gsap } from "gsap";
+import { useEffect, useRef } from "react";
+
+gsap.registerPlugin(useInView);
+
 export interface IFeaturesProps {
   className?: string;
 }
 
 const Features = ({ className, ...props }: IFeaturesProps): JSX.Element => {
+  const imageRef = useRef(null);
+  const contentRef = useRef(null);
+  const { ref, inView } = useInView({
+    threshold: 0.2,
+  });
+
+  useEffect(() => {
+    let imageContainer = imageRef.current;
+    let image = imageContainer?.children[0];
+    console.log("imageDiv and image", imageContainer, image);
+    if (inView) {
+      gsap.to(imageContainer, {
+        opacity: 1,
+        y: 0,
+        duration: 0.5,
+        delay: 0.3,
+      });
+
+      gsap.to(image, {
+        opacity: 1,
+        y: 0,
+        scale: 1,
+        duration: 1,
+        delay: 0.3,
+        ease: "power2.inOut",
+      });
+
+      // gsap.to(contentRef.current, {
+      //   opacity: 1,
+      //   y: 0,
+      //   scale: 1.2,
+      //   duration: 0.2,
+      // });
+    } else {
+      gsap.to(imageContainer, { opacity: 0, y: 20, duration: 0.5 });
+      gsap.to(image, { scale: 1.2, opacity: 0.5, y: 30, duration: 0.2 });
+    }
+  }, [inView]);
+
   return (
-    <div className={styles.features}>
-      <div className={styles["features-image"]}>
+    <div className={styles.features} ref={ref}>
+      <div
+        className={styles["features-image"]}
+        ref={imageRef}
+        style={{ overflow: "hidden", scale: 1.1 }}
+      >
         <img src="/media/taxi-driver.jpg" alt="" />
       </div>
-      <div className={styles["features-content"]}>
+      <div className={styles["features-content"]} ref={contentRef}>
         <h2>
           Real-time work tracking for Riders with an All-in-One solution.{" "}
         </h2>
--- src/components/HeroComponent.tsx
+++ src/components/HeroComponent.tsx
@@ -1,13 +1,44 @@
+import { useEffect, useRef } from "react";
 import SignUpComponent from "./SignUpComponent";
 import styles from "./styles/Hero.module.scss";
+import { useInView } from "react-intersection-observer";
+//@ts-ignore
+import { gsap } from "gsap";
 
 export interface IHeroProps {
   className?: string;
 }
 
 const HeroComponent = ({ className, ...props }: IHeroProps): JSX.Element => {
+  const mockup1 = useRef(null);
+  const mockup2 = useRef(null);
+  const heroImage = useRef(null);
+  const heroImageBg = useRef(null);
+
+  const { ref, inView } = useInView({
+    threshold: 0.4,
+  });
+
+  useEffect(() => {
+    gsap.fromTo(
+      mockup1.current,
+      { opacity: 0, y: 20, duration: 1.4 },
+      { opacity: 1, y: 0, duration: 1, delay: 1.4 },
+    );
+  }, []);
+
+  useEffect(() => {
+    if (inView) {
+      gsap.to(mockup1.current, { opacity: 1, y: 0, duration: 0.5, delay: 0.3 });
+      gsap.to(mockup2.current, { opacity: 1, y: 0, duration: 0.5, delay: 0.5 });
+    } else {
+      gsap.to(mockup1.current, { opacity: 0, y: 10, duration: 1.2 });
+      gsap.to(mockup2.current, { opacity: 0, y: 20, duration: 1 });
+    }
+  }, [inView]);
+
   return (
-    <div className={styles.hero} key="hero">
+    <div className={styles.hero} key="hero" ref={ref}>
       <div className={styles["hero-title"]}>
         <h1>Simplify your work tracking and income management</h1>
         <p>
@@ -20,19 +51,22 @@ const HeroComponent = ({ className, ...props }: IHeroProps): JSX.Element => {
       <div className={styles["hero-subscription"]}>
         <SignUpComponent />
       </div>
-      <div className={styles["hero-image"]}>
+      <div className={styles["hero-image"]} ref={heroImage}>
         <img
           className={styles["hero-image-bg"]}
+          ref={heroImageBg}
           src="/media/uber-rider.jpg"
           alt=""
         />
         <img
           className={styles["hero-image-mockup-1"]}
+          ref={mockup1}
           src="/media/week-mobile-dashboard-mockup.svg"
           alt=""
         />
         <img
           className={styles["hero-image-mockup-2"]}
+          ref={mockup2}
           src="/media/tip-mobile-dashboard-mockup.svg"
           alt=""
         />
